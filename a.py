ad98cc3de8459d8498fbabaf53b90b4a616e39f2
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-09-30 16:59:45 +0700

    first task completed

---
+++ a.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+lst = list(map(int, input().split()))
+
+even_sub = sorted([lst[i] for i in range(len(lst)) if i % 2 == 0])
+odd_sub = sorted([lst[i] for i in range(len(lst)) if i % 2 != 0])
+
+print(max(sum(even_sub) - sum(odd_sub), sum(even_sub) - sum(odd_sub) + 2 * odd_sub[-1] - 2 * even_sub[0]))
+
